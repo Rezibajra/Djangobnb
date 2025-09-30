49e8be96b5909c0beb4a76bffdcd7a4785d83c4c
Rezi Bajracharya <rezi.bajracharya@bajratechnologies.com>
2025-09-30 22:05:06 +0545

    Design implementation for landlord, reservations, myproperties, inbox page

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,10 @@
+const ContactButton = () => {
+
+    return (
+        <div className="mt-6 py-4 px-6 cursor-pointer bg-[#ff385c] hover:bg-[#d50027] text-white rounded-xl transition">
+            Contact
+        </div>
+    )
+}
+
+export default ContactButton;
